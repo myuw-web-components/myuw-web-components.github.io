74030cf65cbc501080028fc7dbae7f490d0907d4
David Witter <david.witter@wisc.edu>
2018-09-04 16:42:15 -0500

    add code generation and adjust style

--- src/interactions.js
+++ src/interactions.js
@@ -1,27 +1,42 @@
 /* Initial properties */
-this.includedComponents = ['search', 'profile', 'drawer'];
+this.includedComponents = ['drawer', 'search', 'profile'];
 
 this.searchCallbackCode = `\ndocument.getElementById('search').callback = (value) => {\n\twindow.alert('You searched for: ' + value);\n}\n\n`;
 
 // Code generation templates
-this.searchTemplate = `
-    <myuw-search id="search" 
+this.appBarTemplateStart = `&lt;myuw-app-bar
+    theme-name="MyUW"
+    app-name="Web Components" 
+    app-url=""&gt;`;
+
+this.appBarTemplateEnd = `&lt;/myuw-app-bar&gt;`;
+
+this.drawerTemplateStart = `&lt;myuw-drawer slot="myuw-navigation"&gt;`
+this.drawerLinkTemplate = `&lt;myuw-drawer-link
+            slot="myuw-drawer-links"
+            name=""
+            icon=""
+            href=""&gt;
+        &lt;/myuw-drawer-link&gt;`;
+
+this.drawerTemplateEnd = `&lt;/myuw-drawer&gt`;
+
+this.searchTemplate = `&lt;myuw-search
         input-label="Search"
         button-label="Submit search"
         icon="search"
-        slot="myuw-search">
-    </myuw-search>
-`;
-this.profileTemplate = `
-    <myuw-profile id="profile" 
+        slot="myuw-search"&gt;
+    &lt;/myuw-search&gt;`;
+
+this.profileTemplate = `&lt;myuw-profile
         slot="myuw-profile"
-        session-endpoint="./demo/session.json"
-        login-url="#"
-        logout-url="#"
-        color="#fb686d">
-        <a href="https://myuw-web-components.github.io/myuw-profile/" slot="nav-item">Profile demo page</a>
-    </myuw-profile>
-`;
+        session-endpoint=""
+        login-url=""
+        logout-url=""&gt;
+        &lt;a href="" slot="nav-item"&gt;&lt;/a&gt;
+    &lt;/myuw-profile&gt;`;
+
+this.customCssTemplate = `&#47;&#42; You didn't change any theme colors &#42;&#47;`;
 
 
 /*
@@ -35,13 +50,14 @@ function updateTitle(e, attribute, newValue) {
 }
 function updateBarBackground(e, property, value) {
     e.preventDefault();
-    var _newCss = document.createElement('style');
-    _newCss.innerHTML = `
-        myuw-app-bar {
-            --${property}: ${value};
-        }
-    `
-    document.head.appendChild(_newCss);
+    this.customCssTemplate = `
+myuw-app-bar {
+    --${property}: ${value};
+}
+    `;
+    var newCss = document.createElement('style');
+    newCss.innerHTML = this.customCssTemplate;
+    document.head.appendChild(newCss);
 }
 function updateTheme(theme) {
     document.body.className = theme;
@@ -50,17 +66,30 @@ function updateTheme(theme) {
     NAV DRAWER DEMO FUNCTIONS
 */
 function addDrawerLink(label, url, icon) {
-    var linkTemplate = `
-        <myuw-drawer-link
+    // Get form elements
+    var linkText = document.getElementById('navItemLabel');
+    var linkUrl = document.getElementById('navItemHref');
+    var linkIcon = document.getElementById('navItemIcon');
+    
+    // Update template for code generation
+    this.drawerLinkTemplate = `&lt;myuw-drawer-link
             slot="myuw-drawer-links"
-            name="${label}"
-            icon="${icon}"
-            href="${url}">
-        </myuw-drawer-link>
-    `;
-    console.log(linkTemplate);
+            name="${linkText.value}"
+            icon="${linkIcon.value}"
+            href="${linkUrl.value}"&gt;
+        &lt;/myuw-drawer-link&gt;`;
+    
+    // Update demo drawer
+    var newLink = document.createElement('myuw-drawer-link');
+    newLink.setAttribute('slot', 'myuw-drawer-links');
+    newLink.setAttribute('name', linkText.value);
+    newLink.setAttribute('icon', linkIcon.value);
+    newLink.setAttribute('href', linkUrl.value);
+
+    document.getElementById('drawer').appendChild(newLink);
+
     // Reset fields and update helper text
-    document.getElementById('drawerHelperText').innerText = `Added "${label}"`;
+    document.getElementById('drawerHelperText').innerText = `Added "${linkText.value}"`;
     document.getElementById('navItemLabel').value = "";
     document.getElementById('navItemHref').value = "";
     document.getElementById('navItemIcon').value = "";
@@ -84,14 +113,12 @@ function updateSearchBar() {
     var icon = document.getElementById('searchIcon');
 
     // Update search template for code generation
-    this.searchTemplate = `
-        <myuw-search id="search" 
-            input-label="${placeholder.value}"
-            button-label="${ariaLabel.value}"
-            icon="${icon.value}"
-            slot="myuw-search">
-        </myuw-search>
-    `;
+    this.searchTemplate = `&lt;myuw-search id="search" 
+        input-label="${placeholder.value}"
+        button-label="${ariaLabel.value}"
+        icon="${icon.value}"
+        slot="myuw-search"&gt;
+    &lt;/myuw-search&gt;`;
 
     // Update populated fields
     if (ariaLabel.value) {
@@ -103,8 +130,6 @@ function updateSearchBar() {
     if (icon.value) {
         searchBar.setAttribute('icon', icon.value);
     }
-
-    searchBar.setAttribute(attribute, value);
 }
 
 function updateCallback(value) {
@@ -160,16 +185,17 @@ function updateProfileTemplate() {
     var linkText = document.getElementById('profileLinkLabel');
     var linkUrl = document.getElementById('profileLinkUrl');
 
+    if (!session.value) {
+        session.value = '';
+    }
     // Update template for code generation
-    this.profileTemplate = `
-        <myuw-profile id="profile" 
-            slot="myuw-profile"
-            session-endpoint="${session.value}"
-            login-url="${login.value}"
-            logout-url="${logout.value}">
-            <a href="${linkUrl.value}" slot="nav-item">${linkText.value}</a>
-        </myuw-profile>
-    `;
+    this.profileTemplate = `&lt;myuw-profile
+        slot="myuw-profile"
+        session-endpoint="${session.value}"
+        login-url="${login.value}"
+        logout-url="${logout.value}"&gt;
+        &lt;a href="${linkUrl.value}" slot="nav-item"&gt;${linkText.value}&lt;/a&gt;
+    &lt;/myuw-profile&gt;`;
     
     // TODO: Print template to page for copy+paste
 
@@ -195,4 +221,79 @@ function toggleComponent(componentId) {
         document.getElementById(componentId + 'ToggleTooltip').innerText = 'Hide ' + componentId;
         this.includedComponents.push(componentId);
     }
+}
+
+/**
+ * Generate all markup necessary for including myuw-web-components
+ * on a page.
+ */
+function generateComponentMarkup() {
+    // Get container elements
+    var importsContainer = document.getElementById('generatedModuleImports');
+    var templateContainer = document.getElementById('generatedTemplate');
+    var cssContainer = document.getElementById('generatedCss');
+    var drawerImport = '';
+    var searchImport = '';
+    var profileImport = '';
+
+    if (this.includedComponents.indexOf('drawer') != -1) {
+        drawerImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-drawer@^1?module"&gt;&lt;/script&gt';
+    }
+    if (this.includedComponents.indexOf('search') != -1) {
+        searchImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-search@^1?module"&gt;&lt;/script&gt';
+    }
+    if (this.includedComponents.indexOf('profile') != -1) {
+        profileImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-profile@^1?module"&gt;&lt;/script&gt';
+    }
+
+    // Include script tags for all components currently toggled ON
+    var importsString = `
+&lt!-- Web component polyfill loader (required to support older browsers) --&gt;
+&lt;script src="https://cdn.rawgit.com/webcomponents/webcomponentsjs/v1/webcomponents-loader.js"&gt;&lt;/script&gt;
+
+&lt!-- UW-Madison app styles (recommended if you're a UW-Madison adopter) --&gt;
+&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-app-styles@^1?module"&gt;&lt;/script&gt;
+
+&lt!-- Import selected myuw-web-components --&gt;
+&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-app-bar@^1?module"&gt;&lt;/script&gt
+${drawerImport}
+${searchImport}
+${profileImport}
+    `;
+    
+    // Build component template string
+    var templateString = `${this.appBarTemplateStart}
+    ${this.drawerTemplateStart}
+        ${this.drawerLinkTemplate}
+    ${this.drawerTemplateEnd}
+    ${this.searchTemplate}
+    ${this.profileTemplate}
+${this.appBarTemplateEnd}
+    `;
+
+    // Update DOM and call syntax highlighter
+    importsContainer.innerHTML = importsString;
+    templateContainer.innerHTML = templateString;
+    cssContainer.innerHTML = this.customCssTemplate;
+    Prism.highlightElement(importsContainer);
+    Prism.highlightElement(templateContainer);
+    Prism.highlightElement(cssContainer);
+}
+
+/**
+ * Copy code to the clipboard
+ * @param {string} id ID of the <code> block to copy
+ */
+function copyMarkup(id) {
+    // Get plain text
+    var snippet = document.getElementById(id).innerText;
+    // Create textarea
+    var selection = document.createElement('textarea');
+    selection.value = snippet;
+    document.body.appendChild(selection);
+    // Select and copy
+    selection.select();
+    document.execCommand('copy');
+    // Remove textarea
+    document.body.removeChild(selection);
 }
