a4f39c3ac1ffc32b313b26db989de7a3b4b8dfe9
David Witter <david.witter@wisc.edu>
2018-10-19 15:11:52 -0500

    add help and feedback component demos

--- src/interactions.js
+++ src/interactions.js
@@ -1,5 +1,5 @@
 /* Initial properties */
-this.includedComponents = ['nav', 'search', 'profile'];
+this.includedComponents = ['nav', 'search', 'profile', 'help'];
 
 this.searchCallbackCode = `\ndocument.getElementById('search').callback = (value) => {\n\twindow.alert('You searched for: ' + value);\n}\n\n`;
 
@@ -34,6 +34,13 @@ this.profileTemplate = `&lt;myuw-profile
         logout-url=""&gt;
         &lt;a href="" slot="nav-item"&gt;&lt;/a&gt;
     &lt;/myuw-profile&gt;`;
+
+this.helpTemplate = `&lt;myuw-help
+        slot="myuw-help"
+        myuw-help-title="Need more help?"
+        show-button&gt;
+        &lt;div&gt;Here's your custom help content&lt;/div&gt;
+    &lt;/myuw-help&gt;`;
 
 this.customCssTemplate = `&#47;&#42; You didn't change any theme colors &#42;&#47;`;
 
@@ -237,6 +244,69 @@ function updateProfileTemplate() {
     document.getElementById('profileHelperText').innerText = 'Updated profile component';
 }
 
+/*
+  HELP AND FEEDBACK DEMO FUNCTIONS
+*/
+
+function filterOutHtmlEntities(htmlInput) {
+  var filteredHtml = htmlInput.replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+  return filteredHtml;
+}
+
+function updateHelpTemplate() {
+  var helpComponent = document.getElementsByTagName('myuw-help')[0];
+  var title = document.getElementById('helpTitle');
+  var showButton = document.getElementById('helpShowButton');
+  var showDefault = document.getElementById('helpShowDefault');
+  var helperText = document.getElementById('helpHelperText');
+  var content = document.getElementById('helpContent');
+
+  // get html out of content
+  var contentValue = filterOutHtmlEntities(content.value);
+
+  // Update component attributes
+  if (title.value.length > 0) {
+    helpComponent.setAttribute('myuw-help-title', title.value);
+  }
+
+  if (showButton.checked) {
+    helpComponent.setAttribute('show-button', 'true');
+  } else {
+    helpComponent.removeAttribute('show-button');
+  }
+
+  if (showDefault.checked) {
+    helpComponent.setAttribute('show-default-content', 'true');
+  } else {
+    helpComponent.removeAttribute('show-default-content');
+  }
+  
+  // Update template for code creation
+  // Update template for code generation
+  this.helpTemplate = `&lt;myuw-help\n\t\tslot="myuw-help"\n\t\tmyuw-help-title="${title.value}"`;
+
+  if (showButton) {
+    this.helpTemplate += `\n\t\tshow-button`;
+  }
+
+  if (showDefault) {
+    this.helpTemplate += `\n\t\tshow-default-content`;
+  }
+
+  this.helpTemplate += `&gt;\n\t\t&lt;div slot="myuw-help-content"&gt;\n\t\t\t${contentValue}\n\t\t&lt;/div&gt;\n\t&lt;/myuw-help&gt;`;
+
+  // Reset form fields
+  title.value = '';
+  showButton.checked = false;
+  showDefault.checked = false;
+  content.value = '';
+  helperText.innerText = 'Updated help component';
+}
+
 /* SHOW/HIDE COMPONENTS */
 function toggleComponent(componentId) {
     if (this.includedComponents.indexOf(componentId) != -1) {
@@ -268,35 +338,60 @@ function generateComponentMarkup() {
     var jsContainer = document.getElementById('generatedJs');
     
     var drawerImport = '';
+    var drawerNoModule = '';
     var searchImport = '';
+    var searchNoModule = '';
     var profileImport = '';
+    var profileNoModule = '';
+    var helpImport = '';
+    var helpNoModule = '';
 
+    
     if (this.includedComponents.indexOf('nav') != -1) {
         drawerImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-drawer@^1?module"&gt;&lt;/script&gt';
+        drawerNoModule = '&lt;script nomodule src="https://unpkg.com/@myuw-web-components/myuw-drawer@^1"&gt;&lt;/script&gt';
     }
     if (this.includedComponents.indexOf('search') != -1) {
         searchImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-search@^1?module"&gt;&lt;/script&gt';
+        searchNoModule = '&lt;script nomodule src="https://unpkg.com/@myuw-web-components/myuw-search@^1"&gt;&lt;/script&gt';
     }
     if (this.includedComponents.indexOf('profile') != -1) {
         profileImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-profile@^1?module"&gt;&lt;/script&gt';
+        profileNoModule = '&lt;script nomodule src="https://unpkg.com/@myuw-web-components/myuw-profile@^1"&gt;&lt;/script&gt';
+    }
+
+    if (this.includedComponents.indexOf('help') != -1) {
+        helpImport = '&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-help@^1?module"&gt;&lt;/script&gt';
+        helpNoModule = '&lt;script nomodule src="https://unpkg.com/@myuw-web-components/myuw-help@^1"&gt;&lt;/script&gt';
     }
 
     // Include script tags for all components currently toggled ON
     var importsString = `
-&lt;!-- Viewport metage tag (required for cross-browser support) --&gt;
+&lt;!-- The following tags are required for cross-browser support --&gt;
 &lt;meta name="viewport" content="width=device-width, initial-scale=1"&gt;
-
-&lt;!-- Web component polyfill loader (required for cross-browser support) --&gt;
 &lt;script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.1.3/webcomponents-loader.js"&gt;&lt;/script&gt;
+&lt;script src="https://unpkg.com/css-vars-ponyfill@1"&gt;&lt;/script&gt;
 
 &lt!-- UW-Madison app styles (recommended if you're a UW-Madison adopter) --&gt;
 &lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-app-styles@^1?module"&gt;&lt;/script&gt;
+&lt;script nomodule src="https://unpkg.com/@myuw-web-components/myuw-app-styles@^1"&gt;&lt;/script&gt;
 
 &lt!-- Import selected myuw-web-components --&gt;
-&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-app-bar@^1?module"&gt;&lt;/script&gt
+&lt;script type="module" src="https://unpkg.com/@myuw-web-components/myuw-app-bar@^1?module"&gt;&lt;/script&gt;
 ${drawerImport}
 ${searchImport}
 ${profileImport}
+${helpImport}
+
+&lt!-- 
+  No-module fallbacks are required for cross-browser support.
+  These will only be loaded in older browser and browsers that don't support ES6 modules.
+--&gt;
+&lt;script nomodule src="https://unpkg.com/@myuw-web-components/myuw-app-bar@^1"&gt;&lt;/script&gt;
+${drawerNoModule}
+${searchNoModule}
+${profileNoModule}
+${helpNoModule}
     `;
     
     // Build component template string, including only markup for visible components
@@ -315,6 +410,10 @@ ${profileImport}
     } else {
       document.getElementById('profileJsDescription').hidden = true;
       jsContainer.hidden = true;
+    }
+
+    if (this.includedComponents.indexOf('help' != -1)) {
+      templateString += `\n\t${this.helpTemplate}`;
     }
     
     templateString += `\n${this.appBarTemplateEnd}`;
